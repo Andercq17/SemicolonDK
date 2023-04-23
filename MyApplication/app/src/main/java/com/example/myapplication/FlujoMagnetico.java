e6c9004f94cad93cf24ff7a24f3cb6cb8f51a661
Andersson <a.cardenas.quiros@gmail.com>
2023-04-23 15:25:22 -0500

    Vistas de magnetismo añadidas en base

---
+++ MyApplication/app/src/main/java/com/example/myapplication/FlujoMagnetico.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlujoMagnetico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flujo_magnetico);
+    }
+}
